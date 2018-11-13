8457f3a5389b1959a0c0797d1cde2aada4f036fe
MartinCarpentier <martin@carpentier.dk>
2018-11-13 22:40:59 +0100

    Added working javascript code, and not working typescript code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ReactTutorial
+
 React tutorial
---
+++ Typescript/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root') as HTMLElement
+);
